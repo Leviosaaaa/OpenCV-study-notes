07bdc27bd1897d6d04358d8af74cd5e40b9c432e
FiniteObliviate <61225697+FiniteObliviate@users.noreply.github.com>
2020-07-28 15:38:47 +0800

    Update

--- 3_Imaging processing/3.3 Image thresholding.py
+++ 3_Imaging processing/3.3 Image thresholding.py
@@ -11,14 +11,17 @@ Created on Tue Jul 28 13:59:56 2020
 了解简单阈值，自适应阈值和Otsu阈值。
 
 【代码内容】
-色彩空间转换
+图像阈值
+P.S.跟官方教程的效果图不太一样，原因还没有搞清楚
 """
 
 import cv2 as cv
 import numpy as np
 from matplotlib import pyplot as plt
 
-img = cv.imread('test2.jpg', 0)
+# In[1]: 简单阈值
+#阈值是全局的
+img = cv.imread('gray_gradient.jpg', 0)
 ret, thresh1 = cv.threshold(img, 127, 255, cv.THRESH_BINARY)
 ret, thresh2 = cv.threshold(img, 127, 255, cv.THRESH_BINARY_INV)
 ret, thresh3 = cv.threshold(img, 127, 255, cv.THRESH_TRUNC)
@@ -26,8 +29,60 @@ ret, thresh4 = cv.threshold(img, 127, 255, cv.THRESH_TOZERO)
 ret, thresh5 = cv.threshold(img, 127, 255, cv.THRESH_TOZERO_INV)
 titles = ['Original Image', 'BINARY', 'BINARY_INV', 'TRUNC', 'TOZERO', 'TOZERO_INV']
 images = [img, thresh1, thresh2, thresh3, thresh4, thresh5]
-for i in xrange(6):
-    plt.subplot(2, 3, i + 1), plt.imshow(image[i], 'gray')
+for i in range(6):
+    plt.subplot(2, 3, i + 1), plt.imshow(images[i], 'gray')
     plt.title(titles[i])
-    plt.xticks([]), plt,yticks([])
+    plt.xticks([]), plt.yticks([])
 plt.show()
+
+# In[2]: 自适应阈值
+#根据图片不同区域的光线条件，给出各个局部的阈值
+img = cv.imread('gray_test.jpg', 0)
+#全局阈值为127
+ret1, th1 = cv.threshold(img, 127, 255, cv.THRESH_BINARY)
+#Otsu阈值
+ret2, th2 = cv.threshold(img, 0, 255, cv.THRESH_BINARY + cv.THRESH_OTSU)
+#Gauss滤波后再采用Otsu阈值
+blur = cv.GaussianBlur(img, (5,5), 0)
+ret3, th3 = cv.threshold(blur, 0, 255, cv.THRESH_BINARY + cv.THRESH_OTSU)
+#绘制所有图像及其直方图
+images = [img, 0,th1,
+          img, 0, th2,
+          blur, 0, th3]
+titles = ['Original Noisy Image', 'Histogram', 'Global Thresholding (v = 127)',
+          'Original Noisy Image', 'Histogram', "Otsu's Thresholding",
+          'Gauss Filtered Image', 'Histogram', "Otsu's Thresholding"]
+for i  in range(3):
+    plt.subplot(3, 3, i*3 + 1), plt.imshow(images[i*3], 'gray')
+    plt.title(titles[i*3]), plt.xticks([]), plt.yticks([])
+    plt.subplot(3,3,i*3 + 2),plt.hist(images[i*3].ravel(),256)
+    plt.title(titles[i*3 + 1]), plt.xticks([]), plt.yticks([])
+    plt.subplot(3,3,i*3 + 3),plt.imshow(images[i*3 + 2],'gray')
+    plt.title(titles[i*3 + 2]), plt.xticks([]), plt.yticks([])
+plt.show()
+
+# In[3]:Otsu二值化的实现
+img = cv.imread('gray_test.jpg', 0)
+blur = cv.GaussianBlur(img,(5,5),0)
+# 寻找归一化直方图和对应的累积分布函数
+hist = cv.calcHist([blur],[0],None,[256],[0,256])
+hist_norm = hist.ravel()/hist.max()
+Q = hist_norm.cumsum()
+bins = np.arange(256)
+fn_min = np.inf
+thresh = -1
+for i in range(1,256):
+    p1,p2 = np.hsplit(hist_norm,[i]) # 概率
+    q1,q2 = Q[i],Q[255]-Q[i] # 对类求和
+    b1,b2 = np.hsplit(bins,[i]) # 权重
+    # 寻找均值和方差
+    m1,m2 = np.sum(p1*b1)/q1, np.sum(p2*b2)/q2
+    v1,v2 = np.sum(((b1-m1)**2)*p1)/q1,np.sum(((b2-m2)**2)*p2)/q2
+    # 计算最小化函数
+    fn = v1*q1 + v2*q2
+    if fn < fn_min:
+        fn_min = fn
+        thresh = i
+# 使用OpenCV函数找到otsu的阈值
+ret, otsu = cv.threshold(blur,0,255,cv.THRESH_BINARY+cv.THRESH_OTSU)
+print( "{} {}".format(thresh,ret) )
--- 3_Imaging processing/3.4 Smoothing images.py
+++ 3_Imaging processing/3.4 Smoothing images.py
@@ -7,10 +7,66 @@ Created on Tue Jul 28 14:04:10 2020
 3.4 Smoothing images
 
 【学习内容】
-使用cv.()。
-
+使用cv.filter2D()
+使用低通滤波器模糊图像。
+注：低通滤波有助于消除高频噪声，是图像模糊；高通滤波有助于在图像中找到边缘。
 
 【代码内容】
-
+图像模糊
 """
+
+import cv2 as cv
+import numpy as np
+from matplotlib import pyplot as plt
+
+
+# In[1]: 2D卷积滤波
+img = cv.imread('logo.png')
+kernel = np.ones((5, 5), np.float32) / 25
+dst = cv.filter2D(img, -1, kernel)
+plt.subplot(121), plt.imshow(img), plt.title('Original')
+plt.xticks([]), plt.yticks([])
+plt.subplot(122), plt.imshow(dst), plt.title('Averaging')
+plt.xticks([]). plt.yticks([])
+plt.show()
+
+
+# In[2]: 平均滤波，中心像素变为一个指定kernel大小的周围像素的平均值
+img = cv.imread('logo.png')
+blur = cv.blur(img,(5,5))
+plt.subplot(121),plt.imshow(img),plt.title('Original')
+plt.xticks([]), plt.yticks([])
+plt.subplot(122),plt.imshow(blur),plt.title('Blurred')
+plt.xticks([]), plt.yticks([])
+plt.show()
+
+
+# In[3]: Gauss模糊，使用Guass核
+img = cv.imread('logo.png')
+blur = cv.GaussianBlur(img,(5,5),0)
+plt.subplot(121),plt.imshow(img),plt.title('Original')
+plt.xticks([]), plt.yticks([])
+plt.subplot(122),plt.imshow(blur),plt.title('Blurred')
+plt.xticks([]), plt.yticks([])
+plt.show()
+
+
+# In[4]: 中位模糊，中心像素变为一个指定kernel大小的周围像素的中位数
+img = cv.imread('logo.png')
+median = cv.medianBlur(img,5)
+plt.subplot(121),plt.imshow(img),plt.title('Original')
+plt.xticks([]), plt.yticks([])
+plt.subplot(122),plt.imshow(median),plt.title('Median')
+plt.xticks([]), plt.yticks([])
+plt.show()
+
+
+# In[5]: 双边滤波，在去除噪声的同时保持边缘尖锐
+img = cv.imread('logo.png')
+blur = cv.bilateralFilter(img,9,75,75)
+plt.subplot(121),plt.imshow(img),plt.title('Original')
+plt.xticks([]), plt.yticks([])
+plt.subplot(122),plt.imshow(blur),plt.title('Blurred')
+plt.xticks([]), plt.yticks([])
+plt.show()
 
