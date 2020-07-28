b67feb98c6ff460b9e81760f65ee3cdc8d7012b6
FiniteObliviate <61225697+FiniteObliviate@users.noreply.github.com>
2020-07-28 16:19:31 +0800

    Update

--- 3_Imaging processing/3.5 Morphological transformations.py
+++ 3_Imaging processing/3.5 Morphological transformations.py
@@ -7,7 +7,7 @@ Created on Tue Jul 28 15:37:21 2020
 3.5 Morphological transformations
 
 【学习内容】
-使用cv.erode(), cv.dilate, cv.morphologyEx()。
+使用cv.erode(), cv.dilate(), cv.morphologyEx()。
 进行腐蚀、膨胀、开运算、闭运算等形态操作。
 
 【代码内容】
@@ -17,24 +17,29 @@ Created on Tue Jul 28 15:37:21 2020
 import cv2 as cv
 import numpy as np
 
-img = cv.imread('test1.jpg', 0)
+img = cv.imread('j.png', 0)
 kernel = np.ones((5, 5), np.uint8)
-#腐蚀
+#腐蚀:孤立的白色区域被腐蚀为黑色
 erosion = cv.erode(img, kernel, iterations = 1)
-#膨胀
+#膨胀：孤立的白色区域膨大
 dilation = cv.dilate(img, kernel, iterations = 1)
-#开运算
 opening = cv.morphologyEx(img, cv.MORPH_OPEN, kernel)
-#闭运算
+#闭运算：膨胀后再腐蚀，常用于去除对象上的黑色斑点
 closing = cv.morphologyEx(img, cv.MORPH_CLOSE, kernel)
-#形态学梯度
+#形态学梯度：给出对象的轮廓
 gradient = cv.morphologyEx(img, cv.MORPH_GRADIENT, kernel)
-#顶帽
+#顶帽：原图与开运算之差
 tophat = cv.morphologyEx(img, cv.MORPH_TOPHAT, kernel)
-#黑帽
+#黑帽：原图与闭运算之差
 blackhat = cv.morphologyEx(img, cv.MORPH_BLACKHAT, kernel)
 
 cv.imshow('erosion', erosion)
+#cv.imshow('dilation', dilation)
+#cv.imshow('opening', opening)
+#cv.imshow('closing', closing)
+#cv.imshow('gradient', gradient)
+#cv.imshow('tophat', tophat)
+#cv.imshow('blackhat', blackhat)
 cv.waitKey(0)
 cv.destroyAllWindows()
 
